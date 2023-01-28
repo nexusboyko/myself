4459b52a3325d243122bc7af7118cf098e5eb8ff
Alex Boyko <alexboyko@outlook.com>
2023-01-27 22:34:34 -0800

    Move AddQuote form to QuotesView component

--- client/src/pages/quotes.js
+++ client/src/pages/quotes.js
@@ -1,15 +1,13 @@
 import React from "react";
-import AddQuote from "../components/AddQuote"
 import QuotesView from "../components/QuotesView"
 
 const Quotes = () => {
   return (
     <>
       <div className="container p-0">
-        <div className="display-6 mb-5">quotes</div>
+        <div className="display-6 mb-5 text-center">quotes</div>
         <div className="quotes rounded-5 p-4">
           <QuotesView />
-          <AddQuote />
         </div>
       </div>
     </>
