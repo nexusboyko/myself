b63ed55a24bf10a64c80d19863848ae2a775e48f
nexusboyko <alexboyko@outlook.com>
2023-12-18 19:42:45 -0800

    Update styles

--- client/app/components/DashboardView.jsx
+++ client/app/components/DashboardView.jsx
@@ -6,9 +6,9 @@ import StretchView from './StretchView';
 
 const DashboardView = (props) => {
   return (
-    <div className='w-[65%] m-auto h-[75vh] flex flex-col justify-center'>
-      <h1 className='text-3xl font-bold mb-10 text-center'>Welcome back, {props.user.name.split(' ')[0]}.</h1>
-      <div id='dashboard' className='flex flex-col gap-y-10'>
+    <div className='w-[80%] m-auto flex flex-col justify-center'>
+      <h1 className='text-3xl font-bold my-10 text-center'>Welcome back, {props.user.name.split(' ')[0]}.</h1>
+      <div id='dashboard' className='flex flex-col gap-y-10 overflow-y-scroll overflow-scroll'>
         <MeditateView />
         <StretchView />
         <QuotesView />
